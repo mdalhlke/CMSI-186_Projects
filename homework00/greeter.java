63273a0208de2077db8df41d7172aaef0d600594
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2018-01-15 11:40:46 -0800

    update code

--- homework00/greeter.java
+++ homework00/greeter.java
@@ -1,5 +1,9 @@
+//@author: Maya Dahlke
+//@date: Jan 13, 2018
+//@errors or warnings: 0
+
 public class Greeter {
   public static void main (String[] args) {   //this can also be String args[]
-    System.out.println(“Hello, world!”);
+    System.out.println("Hello, world!");      //this code prints Hello, world!
   } 
 }
