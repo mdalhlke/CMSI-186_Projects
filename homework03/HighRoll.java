a595ba09e36eaec4df11039edeff4112aa010e1a
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2018-02-15 20:53:51 -0800

    Create HighRoll.java

---
+++ homework03/HighRoll.java
@@ -0,0 +1,22 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  HighRoll.java
+ *  Purpose       :  high roll game using die set
+ *  Author        :  Maya Dahlke
+ *  Date          :  2017-02-15
+ *  Description   :  Implement a Textual User Interface (TUI) on the command line. This will display
+ *                   a list of options to the user, and will prompt for input. Based on that input your 
+ *                   program will do what the user selected, then will display the results, a blank
+ *                   line or two, and then re-display the options.
+ *                   Option 1 in the list must be: ROLL ALL THE DICE
+ *                   Option 2 in the list must be: ROLL A SINGLE DIE
+ *                   Option 3 in the list must be: CALCULATE THE SCORE FOR THIS SET
+ *                   Option 4 in the list must be: SAVE THIS SCORE AS HIGH SCORE
+ *                   Option 5 in the list must be: DISPLAY THE HIGH SCORE
+ *                   Option 6 in the list must be: ENTER 'Q' TO QUIT THE PROGRAM
+ *
+ *  Warnings      :  None
+ *  Exceptions    :  
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+public class HighRoll {
+
+}
