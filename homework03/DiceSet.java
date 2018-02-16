f22cc55009e5eafa7ac66482800a986c6d6a9238
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2018-02-15 20:45:45 -0800

    DiceSet.java methods

--- homework03/DiceSet.java
+++ homework03/DiceSet.java
@@ -3,8 +3,8 @@
  *  Purpose       :  Provides a class describing a set of dice
  *  Author        :  B.J. Johnson
  *  Date          :  2017-02-09
- *  Author        :  Maya Dahke
- *  Date          :  2017-02-06
+ *  Author        :  Maya Dahlke
+ *  Date          :  2017-02-15
  *  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
  *                   idea here is to have an implementing class that uses the Die.java class.  Includes
  *                   the following:
@@ -34,7 +34,7 @@ public class DiceSet {
   /**
    * private instance data
    */
-   private int count;
+   private int count; 
    private int sides;
    private Die[] ds = null;
 
@@ -47,14 +47,28 @@ public class DiceSet {
    * @note   parameters are checked for validity; invalid values throw "IllegalArgumentException"
    */
    public DiceSet( int count, int sides ) {
-      ds = new Die[ count ];
+      if(count < 1 && sides < 4) {
+        throw new IllegalArgumentException("doesn't make sense.");
+      }
+
+      this.ds = new Die[ count ];
+      
+      for(int i = 0; i < count; i++) {
+          this.ds[i] = new Die(sides);
+      }
+      this.sides = sides;
+      this.count = count;
    }
 
   /**
    * @return the sum of all the dice values in the set
    */
    public int sum() {
-      return 0;
+      int add = 0;
+      for(Die d : this.ds) {
+         add += d.getValue();
+      }
+      return add;
    }
 
   /**
@@ -63,53 +77,229 @@ public class DiceSet {
    *  the values of the dice in the set
    */
    public void roll() {
+      for(Die d : this.ds) {
+         d.roll();
+      }
    }
 
   /**
    * Randomly rolls a single die of the dice in this set indexed by 'dieIndex'
    * @param  dieIndex int of which die to roll
    * @return the integer value of the newly rolled die
-   * @trhows IllegalArgumentException if the index is out of range
+   * @throws IllegalArgumentException if the index is out of range
    */
    public int rollIndividual( int dieIndex ) {
-      return 0;
+      if(0 > dieIndex || dieIndex > count - 1) {
+        throw new IllegalArgumentException("index is out of range");
+      }
+      return this.ds[dieIndex].roll();
    }
 
   /**
    * Gets the value of the die in this set indexed by 'dieIndex'
    * @param  dieIndex int of which die to roll
-   * @trhows IllegalArgumentException if the index is out of range
+   * @throws IllegalArgumentException if the index is out of range
    */
    public int getIndividual( int dieIndex ) {
-      return -999;
+      if(0 > dieIndex || dieIndex > count - 1) {
+        throw new IllegalArgumentException("index is out of range");
+      }
+      return this.ds[dieIndex].getValue();
    }
 
   /**
    * @return Public Instance method that returns a String representation of the DiceSet instance
    */
    public String toString() {
-      String result = "";
-      return result;
+      StringBuffer buffer = new StringBuffer();
+      for(Die d : this.ds) {
+        buffer.append(d.toString());
+      }
+      return buffer.toString();
    }
 
   /**
    * @return Class-wide version of the preceding instance method
    */
    public static String toString( DiceSet ds ) {
-      return "";
+      return ds.toString();
    }
 
   /**
-   * @return  tru iff this set is identical to the set passed as an argument
+   * @return  true if this set is identical to the set passed as an argument
    */
    public boolean isIdentical( DiceSet ds ) {
-      return true;
+      return (this.count == ds.count && this.sides == ds.sides);
    }
   /**
    * A little test main to check things out
    */
    public static void main( String[] args ) {
-      // You do this part!
+      //testing sum() method
+      System.out.println( "Testing sum() method:" );
+
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(6, 4); System.out.println( d.sum() <= 24 ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 8); System.out.println( d.sum() <= 40 ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 4); System.out.println( d.sum() <= 20 ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(8, 6); System.out.println( d.sum() <= 48 ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(7, 5); System.out.println( d.sum() <= 35 ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+     
+      //testing rollIndividual() method
+      System.out.println( "Testing rollIndividual() method:" ); 
+
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(6, 4); System.out.println( d.rollIndividual(3) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 4); System.out.println( d.rollIndividual(2) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(7, 8); System.out.println( d.rollIndividual(6) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 9); System.out.println( d.rollIndividual(4) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(4, 6); System.out.println( d.rollIndividual(3) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+     
+      //testing getIndividual() method
+      System.out.println( "Testing getIndividual() method:" ); 
+
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(6, 4); System.out.println( d.getIndividual(3) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 4); System.out.println( d.getIndividual(2) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(7, 8); System.out.println( d.getIndividual(6) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(9, 5); System.out.println( d.getIndividual(7) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 7); System.out.println( d.getIndividual(4) <= d.sides ? "true" : "false" );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+     
+       
+      //testing toString() method
+      System.out.println( "testing toString() method:" ); 
+
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(6, 4); System.out.println( d.toString());}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 7); System.out.println( d.toString());}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(9, 5); System.out.println( d.toString());}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(4, 5); System.out.println( d.toString());}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(8, 6); System.out.println( d.toString());}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+    
+      
+      //testing toString( DiceSet ds )
+      System.out.println( "Testing toString( DiceSet ds ) method:" ); 
+
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(6, 4); System.out.println( DiceSet.toString(d) );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(5, 7); System.out.println( DiceSet.toString(d) );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(9, 5); System.out.println( DiceSet.toString(d) );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(4, 5); System.out.println( DiceSet.toString(d) );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+     
+      System.out.print( "should get true:" ); 
+      try { DiceSet d = new DiceSet(8, 6); System.out.println( DiceSet.toString(d) );}                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+      
+     
+      //testing isIdentical()
+      System.out.println( "Testing isIdentical() method:" ); 
+
+      System.out.print( "should get true:" ); 
+      try { 
+         DiceSet d1 = new DiceSet(6, 4);
+         DiceSet d2 = new DiceSet(5, 7); 
+         System.out.println( d1.isIdentical(d2) == false ? "true" : "false" );
+      }                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+
+      System.out.print( "should get true:" ); 
+      try { 
+         DiceSet d1 = new DiceSet(6, 4);
+         DiceSet d2 = new DiceSet(6, 4); 
+         System.out.println( d1.isIdentical(d2) == true ? "true" : "false" );
+      }                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+
+      System.out.print( "should get true:" ); 
+      try { 
+         DiceSet d1 = new DiceSet(8, 8);
+         DiceSet d2 = new DiceSet(5, 8); 
+         System.out.println( d1.isIdentical(d2) == false ? "true" : "false" );
+      }                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+
+      System.out.print( "should get true:" ); 
+      try { 
+         DiceSet d1 = new DiceSet(5, 7);
+         DiceSet d2 = new DiceSet(5, 7); 
+         System.out.println( d1.isIdentical(d2) == true ? "true" : "false" );
+      }                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+
+      System.out.print( "should get true:" ); 
+      try { 
+         DiceSet d1 = new DiceSet(5, 7);
+         DiceSet d2 = new DiceSet(7, 5); 
+         System.out.println( d1.isIdentical(d2) == false ? "true" : "false" );
+      }                                  
+      catch( Exception e ) { System.out.println ( " number can't be used" ); }
+
+
    }
 
 }
