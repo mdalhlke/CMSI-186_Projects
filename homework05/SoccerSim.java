b14281704663d7eaf3dc649a2369484d9335b039
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2018-03-20 14:59:36 -0700

    create SoccerSim.java

---
+++ homework05/SoccerSim.java
@@ -0,0 +1,20 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SoccerSim.java
+ *  Purpose       :  Simultaneously, on a perfectly flat playground, at time 00:00:00.0 (hrs:mins:secs), an arbitrary number of soccer balls are kicked, 
+ *                   all at different speeds and directions. We wish to find out, via a discrete simulation of the system, whether a collision will ever 
+ *                   take place, and, if so, where and when. Each ball has a radius of 4.45 inches and weighs one pound. The center of the playground is
+ *                   presumed to be the point (x,y) = (0.0,0.0).
+ *  author        :  Maya Dahlke
+ *  Date started  :  2018-03-13
+ *  Description   :  This class provides a bunch of methods for SoccerSim.class
+ *
+ *  Notes         :  None right now.  I'll add some as they occur.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class SoccerSim {
+	public SoccerSim() {
+		super();
+	}
+}
