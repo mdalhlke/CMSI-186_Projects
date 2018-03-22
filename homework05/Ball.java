bb36439abf13d534c97536423327a3540ecf8aa7
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2018-03-21 21:21:15 -0700

    update methods

--- homework05/Ball.java
+++ homework05/Ball.java
@@ -11,11 +11,12 @@
  *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
 public class Ball {
     
-   private double rx, ry;
-   private double vx, vy;
+   private double rx;
+   private double ry;
+   private double vx;
+   private double vy;
    private double timeslice;
    private final double radius = 4.50; //in inches
    //private final double weight = 1; //in pounds
@@ -29,47 +30,50 @@ public class Ball {
       this.timeslice = Double.parseDouble(timeslice);
    }
 
-   //methods
-   public String[] locationBall() {
-      //how to determine location
-      String loc[] = new String[2];
-      loc[0] = String.valueOf(this.rx);
-      loc[1] = String.valueOf(this.ry);
-      System.out.println(loc);
-      return loc;
+   //gets the location in the x direction
+   public double getLocationX() {
+      return this.rx; 
    }
 
-   public void moveBall() {
-      //move to another location using velocity
-      if (Math.abs(this.rx + this.vx) + radius > 1.0) System.exit(1);
-      if (Math.abs(this.ry + this.vy) + radius > 1.0) System.exit(1);
-      double rxf, ryf;
-      rxf = this.rx + this.vx;
-      ryf = this.ry += this.vy;
-
+   //gets the location in the y direction
+   public double getLocationY() {
+      return this.ry;
    }
 
-   public void friction() {
-       //slows each ball until the ball comes to rest - friction is a force that continuously decreases ball's speed at 1%/sec until
-   	   //the ball is traveling less than one inch per sec, at which point it comes to rest.
-   	      double vxf = this.vx * (Math.pow(0.99, this.timeslice));
-   	      double vyf = this.vy * (Math.pow(0.99, this.timeslice));
-   	      if (this.rx == 0 && this.ry == 0) {
-   	         System.out.println("Ball is at rest");
-   	         System.exit(1);
-   	      }
+   //decreases the velocity due to the frictional force until the ball comes to rest
+   public void velocityDueToFriction() {
+      this.vx = this.vx * (Math.pow(0.99, this.timeslice));
+   	  this.vy = this.vy * (Math.pow(0.99, this.timeslice));
+   	  double vf = Math.sqrt(this.vx * this.vx + this.vy * this.vy); 
+   	  if (vf <= 1/12) {
+   	      this.vx = 0;
+   	      this.vy = 0;
+   	  } 
    }
 
+   //moves the ball to another location according to the velocity  
+   public void move() {
+       this.velocityDueToFriction();
+       this.rx = this.vx * this.timeslice + this.rx;
+       this.ry = this.vy * this.timeslice + this.ry;
+
+
+   //converts the results into a string 
    public String toString() {
-      return "hi";
+      //locationBall();
+     StringBuffer buffer = new StringBuffer();
+     return buffer.append(this.rx).append("\n").append(this.ry).append("\n").append(this.vx).append("\n").append(this.vy).append("\n").toString();
    }
 
+   //testing
    public static void main(String[] args) {
       System.out.println( "\nBALL CLASS TESTER PROGRAM\n" + "--------------------------\n" );
   
-      System.out.println("Testing locationBall() method:");
+      Ball ball = new Ball("0", "0", "1", "0", "1");
+      ball.move();
+      System.out.println(ball.toString());
+      /*System.out.println("Testing locationBall() method:");
       try { Ball ball = new Ball("3", "4", "5", "6", "0.01"); System.out.println(("3,4" == ball.locationBall()) ? " - got it" : " - no joy" ); }
-      catch( Exception e ) { System.out.println ( " - Exception thrown: " + e.toString() ); }
+      catch( Exception e ) { System.out.println ( " - Exception thrown: " + e.toString() ); }*/
    }
 }
-
